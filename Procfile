3179f4f6fa85c733eb42268514eb424fb60f1aea
Ivan Kolesnikov <i.kolesnikov@ritg.ru>
2022-01-07 09:50:01 +0300

    fix deploy

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-release: python manage.py migrate
-release: python manage.py collectstatic --noinput
 web: cd /app/frontend && npm run build && npm run start
 server: cd /app/backend && gunicorn backend.config.wsgi --log-file -
+release: python manage.py migrate
+release: python manage.py collectstatic --noinput
--- frontend/src/component/PrimaryCurators/PrimaryCurators.js
+++ frontend/src/component/PrimaryCurators/PrimaryCurators.js
@@ -5,21 +5,21 @@ const PrimaryCurators = () => (
     </div>
     <a className="cooking" href="/cooking">
       <div className="curatorsImg cookingImg">
-        <img alt="cooking" src="../img/primaryCurators/img1.svg" />
+        <img alt="cooking" src="./img/PrimaryCurators/img1.svg" />
       </div>
       <div className="curatorsName">Дмитрий Левов</div>
       <div className="curatorsDescription">Архитектор о современных тендециях сферы</div>
     </a>
     <a className="cosiness" href="/cosiness">
       <div className="curatorsImg cosinessImg">
-        <img alt="cosiness" src="./img/primaryCurators/img2.svg" />
+        <img alt="cosiness" src="./img/PrimaryCurators/img2.svg" />
       </div>
       <div className="curatorsName">Мария Серединкова</div>
       <div className="curatorsDescription">Самый популярный шеф-повар страны делится кулинарными секретами </div>
     </a>
     <a className="fashion" href="/fashion">
       <div className="curatorsImg fashionImg">
-        <img alt="fashion" src="./img/primaryCurators/img3.svg" />
+        <img alt="fashion" src="./img/PrimaryCurators/img3.svg" />
       </div>
       <div className="curatorsName">Изольда Правова</div>
       <div className="curatorsDescription">Дизайнер одежды рассказывает про моду человеческим языком</div>
