34235792b7b05752a4ba48eaa4ac34b79d75239c
TungusMur <vova.myraw@gmail.com>
2022-01-11 14:52:10 +0400

    change LogIn

--- frontend/src/Store/reducers/reducerAuthorization.js
+++ frontend/src/Store/reducers/reducerAuthorization.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const AUTHORIZATION = 'AUTHORIZATION';
 export const LOG_OUT = 'LOG_OUT';
 export const LOG_IN = 'LOG_IN';
+export const LOG_IN_ACCESS = 'LOG_IN_ACCESS';
+export const LOG_IN_REFRESH = 'LOG_IN_REFRESH';
 
 const defaultState = {
   status: [],
@@ -17,6 +19,16 @@ export default (state = defaultState, { type, payload }) => {
         status: payload,
       };
     case LOG_IN:
+      return {
+        ...state,
+        status: payload,
+      };
+    case LOG_IN_ACCESS:
+      return {
+        ...state,
+        status: payload,
+      };
+    case LOG_IN_REFRESH:
       return {
         ...state,
         status: payload,
@@ -55,21 +67,53 @@ export const logOut = () => (dispatch) => {
 };
 
 export const logIn = () => (dispatch) => {
-  const token = JSON.parse(localStorage.getItem('token'));
-  axios
-    .get('https://bloglab-backend.herokuapp.com/api/users/current_user/', {
-      headers: { Authorization: `Bearer ${token.access}` },
-    })
-    .then((res) => {
-      dispatch({
-        type: LOG_IN,
-        payload: res.status,
-      });
-    })
-    .catch((error) =>
-      dispatch({
-        type: LOG_IN,
-        payload: error,
-      })
-    );
+  try {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (token && token.access) {
+      axios
+        .get('https://bloglab-backend.herokuapp.com/api/users/current_user/', {
+          headers: { Authorization: `Bearer ${token?.access}` },
+        })
+        .then((res) => {
+          dispatch({
+            type: LOG_IN_ACCESS,
+            payload: res.status,
+          });
+        })
+        .catch((errorAccess) => {
+          dispatch({
+            type: LOG_IN_ACCESS,
+            payload: errorAccess,
+          });
+
+          axios
+            .get('https://bloglab-backend.herokuapp.com/api/token/refresh/', {
+              refresh: token?.refresh,
+            })
+            .then((res) => {
+              localStorage.setItem('token', JSON.stringify({ ...token, ...res.data }));
+
+              dispatch({
+                type: LOG_IN_REFRESH,
+                payload: res.status,
+              });
+
+              logIn();
+            })
+            .catch((error) => {
+              dispatch({
+                type: LOG_IN_REFRESH,
+                payload: error,
+              });
+
+              logOut();
+            });
+        });
+    } else {
+      localStorage.token = JSON.stringify({ token: null });
+      dispatch({ type: LOG_OUT });
+    }
+  } catch {
+    logIn();
+  }
 };
